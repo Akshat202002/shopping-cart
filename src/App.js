688e998813c14b6d40292437b70882552515fb8a
Akshat202002 <akshatsamdani23@gmail.com>
2022-10-03 20:42:15 +0530

    basic shopping cart functionalities

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Cart from "./Cart";
+
+const items = [
+  { id: 1, name: "Cashews", price: 2.25, qty: 2 },
+  { id: 2, name: "Chocolates", price: 1.99, qty: 3 },
+  { id: 3, name: "Strawberries", price: 3.5, qty: 10 },
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Cart item={items} />;
 }
 
 export default App;
